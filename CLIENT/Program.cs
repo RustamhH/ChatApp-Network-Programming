bc43ed942e5d7359741601036d7f3288ba4bf4df
RustamhH <123383618+RustamhH@users.noreply.github.com>
2023-12-10 22:00:16 +0400

    Salam

---
+++ CLIENT/Program.cs
@@ -0,0 +1,44 @@
+﻿// client
+
+using System.Net.Sockets;
+using System.Net;
+
+var port = 27001;
+
+
+var client = new TcpClient("192.168.100.8",port);
+
+
+
+var stream = client.GetStream();
+
+var binaryReader = new BinaryReader(stream);
+var binaryWriter = new BinaryWriter(stream);
+Task.Run(async () =>
+{
+
+    Console.Write("Your Name: ");
+    var name = Console.ReadLine();
+    binaryWriter.Write(name);
+
+    while (true)
+    {
+        Console.Write($"To: ");
+
+        var ToWho = Console.ReadLine();
+
+        Console.Write($"Message: ");
+
+        var Message = Console.ReadLine();
+        binaryWriter.Write(ToWho + " " + Message);
+    }
+
+});
+
+while (true)
+{
+    var Mes = binaryReader.ReadString();
+    if (!string.IsNullOrEmpty(Mes)) Console.WriteLine(Mes);
+
+}
+
---
+++ SERVER/Program.cs
@@ -0,0 +1,75 @@
+﻿
+
+
+// server
+
+using System.Net;
+using System.Net.Sockets;
+
+#pragma warning disable
+
+var ip = IPAddress.Parse("192.168.100.8");
+var port = 27001;
+
+var listener = new TcpListener(ip, port);
+
+Dictionary<string, TcpClient> clients = new Dictionary<string, TcpClient>();
+
+
+listener.Start(10);
+
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+
+    Task.Run(() =>
+    {
+        bool isfirst = true;
+        var clientStream = client.GetStream();
+
+        var binaryReader = new BinaryReader(clientStream);
+        
+        var readString = "";
+        var tousername = "";
+        var index = 0;
+        
+        var username = "";
+        
+        if (isfirst)
+        {
+            username = binaryReader.ReadString();
+            Console.WriteLine($"{username} Connected.... ");
+
+            clients.Add(username, client);
+
+            isfirst = false;
+        }
+
+
+
+        while (true)
+        {
+            readString = binaryReader.ReadString();
+            index = readString.IndexOf(" ");
+            tousername = readString.Substring(0, index);
+
+
+            var qebulEden = clients.FirstOrDefault(c => c.Key.ToString() == tousername).Value;
+            if (qebulEden is not null)
+            {
+
+                var qebulEdenStream = qebulEden.GetStream();
+                var binaryWriter = new BinaryWriter(qebulEdenStream);
+
+
+                var sendMsg = username+ ":  " + readString.Substring(index + 1);
+
+                binaryWriter.Write(sendMsg);
+            }
+
+        }
+
+    });
+
+}
